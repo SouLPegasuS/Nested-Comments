59704afb7ef48d08b315d6b18e297f26ae760cc9
SouLPegasuS <pratham7910@gmail.com>
2021-07-16 16:03:23 +0530

    created project folder structure

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+const App = () => {
+  return (<h1> hi there </h1>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path = "/" component = {Login} />
+    //   </Switch>
+    // </Router>
+  );
+}
+export default App;
