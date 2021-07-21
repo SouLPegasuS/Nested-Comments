84ac161e3217cb52db43ffcb74273767a92c942c
SouLPegasuS <pratham7910@gmail.com>
2021-07-21 23:56:07 +0530

    merged css files

--- client/src/components/comment.jsx
+++ client/src/components/comment.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Input, Button, Typography } from '@material-ui/core';
-import "./Comment.css";
 
 const Comment = (props) => {
     const [editClicked, setEditClicked] = useState(false);
